7f797e73b02ad1a95f5f780b451a79bba1f869d0
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-04 10:41:50 +0530

    [Praveen] Add: Added the Contact Detail Class

---
+++ ContactDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblem
+{
+    class ContactDetails
+    {
+        public string firstName;
+        public string secondName;
+        public string address;
+        public string city;
+        public string state;
+        public long zip;
+        public long phoneNumber;
+        public string emailId;
+
+        public ContactDetails(string firstName, string secName, string address, string city, string state, long zip, long phoneNumber, string emailId)
+        {
+            this.firstName = firstName.ToLower();
+            this.secondName = secName.ToLower();
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.emailId = emailId;
+        }
+    }
+}
