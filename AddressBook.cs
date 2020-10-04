7e44e7854f09946ecaa85d5d638e5114dea3edcc
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-04 11:11:05 +0530

    [Praveen] Add: Added the feature to edit the contact in list

--- AddressBook.cs
+++ AddressBook.cs
@@ -79,5 +79,54 @@ namespace AddressBookProblem
             }
 
         }
+
+        public void editContactDetails()
+        {
+            int index = 0;
+
+            Console.WriteLine("Enter the first name of person whose data to be modified=");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter the second name of person whose data to be modified=");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the modified data===>");
+
+            Console.WriteLine("Enter the first name=");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter the second name=");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Enter the address=");
+            address = Console.ReadLine();
+            Console.WriteLine("Enter the City=");
+            city = Console.ReadLine();
+            Console.WriteLine("Enter the State=");
+            state = Console.ReadLine();
+            Console.WriteLine("Enter the zip code=");
+            zip = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the Phone number=");
+            phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the emailId=");
+            email = Console.ReadLine();
+
+            foreach (var contactObj in this.contactList)
+            {
+
+                if ((firstName == contactObj.firstName) && (lastName == contactObj.secondName))
+                {
+                    break;
+                }
+                index++;
+            }
+
+            contactList[index].firstName = firstName;
+            contactList[index].secondName = lastName;
+            contactList[index].address = address;
+            contactList[index].city = city;
+            contactList[index].state = state;
+            contactList[index].zip = zip;
+            contactList[index].phoneNumber = phoneNumber;
+            contactList[index].emailId = email;
+        }
     }
 }
+
